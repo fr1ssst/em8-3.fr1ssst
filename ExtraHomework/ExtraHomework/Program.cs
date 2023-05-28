ebd6495308bd2847108e89fc6bd55035098394d4
fr1ssst <ratyshnui2008@gmail.com>
2023-05-28 13:14:39 +0300

    Update fail

--- ExtraHomework/ExtraHomework/Program.cs
+++ ExtraHomework/ExtraHomework/Program.cs
@@ -3,12 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExtraHomework
 {
-    class EH
+    class MessageHelper
     {
+        static public string getString(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        static public int getInt(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            return Convert.ToInt32(MessageHelper.getString(message));
+        }
+
+        static public double getDouble(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            return Convert.ToDouble(MessageHelper.getString(message));
+        }
+
+        static public bool getBool(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            return Convert.ToBoolean(MessageHelper.getString(message));
+        }
         public void showText(string text, ConsoleColor color = ConsoleColor.White)
         {
             Console.ForegroundColor = color;
@@ -16,105 +41,131 @@ namespace ExtraHomework
 
             Console.WriteLine();
         }
+    }
+    class Task
+    {
+        MessageHelper mh = null;
         public void NumberOne()
         {
-            string str = "";
-            str = Convert.ToString(Console.ReadLine());
+            mh = new MessageHelper();
+            string numbers = MessageHelper.getString("Введіть цифри:", ConsoleColor.Green);
 
-            string[] words = str.Split(new[] { ' ' }, StringSplitOptions.None);
-
-            string word = "";
-            int c = 0;
-
-            foreach(string strR in words) 
-            {
-                if(str.Length > c) 
-                { 
-                    word = strR;
-                    c = strR.Length;
-                }
+            mh.showText("Список з унікальними цифри:", ConsoleColor.Yellow);
+            var i = numbers.Split(' ');
+            var unique_numbers = new HashSet<string>(i);
+            foreach (string n in unique_numbers)
+            { 
+                Console.WriteLine(n); 
             }
-
-            Console.WriteLine(word);
         }
         public void NumberTwo()
-        {
-            var text = "123, 67, 456, 22";
-            var sum =
-                text
-                    .Split(new[] { ',' })
-                    .Select(x => int.Parse(x.Trim()))
-                    .Sum();
-            Console.WriteLine(sum);
+        { 
+            mh = new MessageHelper();
+
+            string word = MessageHelper.getString("Напишіть слово(На українській мові):", ConsoleColor.Blue);
+            int vowels = 0;
+            int consonants = 0;
+            int len = word.Length;
+
+            for (int i = 0; i < len; i++)
+            {               
+                if (word[i] == 'а' || word[i] == 'е' ||
+                    word[i] == 'є' || word[i] == 'и' ||
+                    word[i] == 'і' || word[i] == 'ї' ||
+                    word[i] == 'й' || word[i] == 'о' ||
+                    word[i] == 'у' || word[i] == 'ь' || 
+                    word[i] == 'ю' || word[i] == 'я')
+                {
+
+                    vowels++;
+                }
+                else if ((word[i] >= 'а' && word[i] <= 'я') ||
+                         (word[i] >= 'А' && word[i] <= 'Я'))
+                {
+                    consonants++;
+                }
+            }
+            mh.showText($"Кількість голосних = {vowels}", ConsoleColor.Yellow);
+            mh.showText($"Кількість приголосних = {consonants}", ConsoleColor.Cyan);
         }
         public void NumberThree()
         {
-            string str1 = "";
-            string str2 = "";
+            mh = new MessageHelper();
+            int sum = 0;
 
-            str1 = Convert.ToString(Console.ReadLine());
-            str2 = Convert.ToString(Console.ReadLine());
+            int numbes = MessageHelper.getInt("Напишіть число(ціле):", ConsoleColor.Magenta);
+            while (numbes != 0)
+            {
+                sum += numbes % 10;
+                numbes /= 10;
+            }
+            mh.showText($"Сума цифр зазначеного цілого числа: {sum}", ConsoleColor.DarkYellow);
         }
         public void NumberFour() 
         {
-            int n = 0;
-            int flag = 0;
-            showText("Напишіть число:", ConsoleColor.Magenta);
-            n = Convert.ToInt32(Console.ReadLine());
-
-            for( int i = 2; i <= n/2; i++ ) 
-            { 
-                if(n % 2 == 0) 
-                {
-                    flag = 1;
-                }
-            }
-            if(flag == 1) 
-            {
-                showText($"Число {n} не просте число", ConsoleColor.Red);
-            }
-            else 
-            {
-                showText($"Число {n} просте число", ConsoleColor.Green);
-            }
+            mh = new MessageHelper();
         }
     }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            EH eh = new EH();
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
 
-            eh.showText("Виберіть завдання:", ConsoleColor.White);
-            eh.showText("[1] Завдання №1", ConsoleColor.Cyan);
-            eh.showText("[2] Завдання №2", ConsoleColor.Green);
-            eh.showText("[3] Завдання №3", ConsoleColor.Yellow);
-            eh.showText("[4] Завдання №4", ConsoleColor.Blue);
-            int menuM = Convert.ToInt32(Console.ReadLine());
+            MessageHelper mh = new MessageHelper();
+            Task task = new Task();
+
+            mh.showText("Виберіть завдання:", ConsoleColor.White);
+            mh.showText("[1] Завдання №1", ConsoleColor.Cyan);
+            mh.showText("[2] Завдання №2", ConsoleColor.Green);
+            mh.showText("[3] Завдання №3", ConsoleColor.Yellow);
+            //mh.showText("[4] Завдання №4", ConsoleColor.Blue);
+            mh.showText("[0] Вихід з програми", ConsoleColor.Red);
+            int menuM = MessageHelper.getInt("");
 
             Console.Clear();
 
             switch (menuM)
             {
+                case 0:
+                    mh.showText("Вихід із програми...", ConsoleColor.DarkRed);
+                    Thread.Sleep(1000);
+                    return;
                 case 1:
-                    eh.NumberOne();
+                    mh.showText("Умова:" +
+                        "\n1)Створіть програму, яка приймає на вхід список " +
+                        "чисел і повертає новий список, що містить лише " +
+                        "унікальні числа з початкового списку, без повторень.", ConsoleColor.Gray);
+                    task.NumberOne();
                     break;
                 case 2:
-                    eh.NumberTwo();
+                    mh.showText("Умова:" +
+                        "\n2)Напишіть програму, яка приймає на вхід рядок і " +
+                        "повертає кількість голосних і приголосних літер у " +
+                        "цьому рядку.", ConsoleColor.Gray);
+                    task.NumberTwo();
                     break;
                 case 3:
-                    eh.NumberThree();
+                    mh.showText("Умова:" +
+                        "\n3)Створіть програму, яка приймає на вхід число " +
+                        "і повертає суму всіх цифр цього числа.", ConsoleColor.Gray);
+                    task.NumberThree();
                     break;
-                case 4:
-                    eh.NumberFour();
-                    break;
+                /*case 4:
+                   mh.showText("Умова:" +
+                        "\n4)", ConsoleColor.Gray);
+                    task.NumberFour();
+                    break;*/
                 default:
-                    eh.showText("Виберіть завдання:", ConsoleColor.White);
-                    eh.showText("[1] Завдання №1", ConsoleColor.Cyan);
-                    eh.showText("[2] Завдання №2", ConsoleColor.Green);
-                    eh.showText("[3] Завдання №3", ConsoleColor.Yellow);
-                    menuM = Convert.ToInt32(Console.ReadLine());
+                    mh.showText("Виберіть завдання:", ConsoleColor.White);
+                    mh.showText("[1] Завдання №1", ConsoleColor.Cyan);
+                    mh.showText("[2] Завдання №2", ConsoleColor.Green);
+                    mh.showText("[3] Завдання №3", ConsoleColor.Yellow);
+                    //mh.showText("[4] Завдання №4", ConsoleColor.Blue);
+                    mh.showText("[0] Вихід з програми", ConsoleColor.Red);
+                    menuM = MessageHelper.getInt("");
+
 
                     Console.Clear();
                     break;
--- Homework/Homework/Program.cs
+++ Homework/Homework/Program.cs
@@ -2,12 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace HomeWork
+namespace Homework
 {
-    class E
+    class MessageHelper
     {
+        static public string getString(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            return Console.ReadLine();
+        }
+
+        static public int getInt(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            return Convert.ToInt32(MessageHelper.getString(message));
+        }
+
+        static public double getDouble(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            return Convert.ToDouble(MessageHelper.getString(message));
+        }
+
+        static public bool getBool(string message, ConsoleColor color = ConsoleColor.White)
+        {
+            Console.ForegroundColor = color;
+            return Convert.ToBoolean(MessageHelper.getString(message));
+        }
         public void showText(string text, ConsoleColor color = ConsoleColor.White)
         {
             Console.ForegroundColor = color;
@@ -15,213 +41,81 @@ namespace HomeWork
 
             Console.WriteLine();
         }
+    }
+    class Task
+    {
+        MessageHelper mh = null;
         public void NumberOne()
         {
-            showText("•Введіть свій зріст:", ConsoleColor.Cyan);
-            double h_cen = Convert.ToDouble(Console.ReadLine());
-
-            showText("•Введіть масу свого тіла:", ConsoleColor.DarkYellow);
-            double m = Convert.ToDouble(Console.ReadLine());
-
-            double h_meters = h_cen / 100;
-            double imt = m / Math.Pow(h_meters, 2);
-
-            showText($"•Твій IMT: {imt}", ConsoleColor.Green);
-            showText($"---------------------------------", ConsoleColor.White);
-            if (imt < 18.5)
-            {
-                showText("•У вас недостатня маса", ConsoleColor.Magenta);
-            }
-            else if (imt > 18.5 && imt < 25)
-            {
-                showText("•У вас нормальна маса", ConsoleColor.Green);
-            }
-            else if (imt > 24.9 && imt < 30)
-            {
-                showText("•У вас передожиріння (гладкість)", ConsoleColor.Yellow);
-            }
-            else if (imt > 29.9 && imt < 35)
-            {
-                showText("•У вас ожиріння I ступеня", ConsoleColor.DarkYellow);
-            }
-            else if (imt > 34.9 && imt < 40)
-            {
-                showText("•У вас ожиріння II ступеня", ConsoleColor.Red);
-            }
-            else if (imt > 40)
-            {
-                showText("•У вас ожиріння III ступеня", ConsoleColor.DarkRed);
-            }
-            showText($"---------------------------------", ConsoleColor.White);
+            mh = new MessageHelper();           
         }
         public void NumberTwo()
         {
-            showText("Виберіть одиниці виммірювання:", ConsoleColor.Cyan);
-            showText("[1] Часу", ConsoleColor.Cyan);
-            showText("[2] Довжини", ConsoleColor.Cyan);
-            showText("[3] Маси", ConsoleColor.Cyan);
-            int menu = Convert.ToInt32(Console.ReadLine());
-            Console.Clear();
-
-            showText("Напишіть число", ConsoleColor.Green);
-            int num = Convert.ToInt32(Console.ReadLine());
-            showText("-------------------------------", ConsoleColor.White);
-
-            switch (menu)
-            {
-                case 1:
-                    showText($"{num} хв = {num * 60} секунд", ConsoleColor.Yellow);
-                    showText($"{num} год = {num * 60} хв", ConsoleColor.Green);
-                    showText($"{num} доб = {num * 24} год", ConsoleColor.Cyan);
-                    showText($"{num} тиж = {num * 7} діб", ConsoleColor.Magenta);
-                    showText($"{num} міс = {num * 31} діб", ConsoleColor.Red);
-                    showText($"{num} рік = {num * 365} діб", ConsoleColor.Blue);
-                    break;
-                case 2:
-                    showText($"{num} см = {num * 10} мм", ConsoleColor.Yellow);
-                    showText($"{num} дм = {num * 10} см", ConsoleColor.Green);
-                    showText($"{num} м = {num * 10} дм", ConsoleColor.Cyan);
-                    showText($"{num} м = {num * 100} см", ConsoleColor.Magenta);
-                    showText($"{num} км = {num * 1000} м", ConsoleColor.Red);
-                    showText($"{num} см = {num * 0.393701} дюйм", ConsoleColor.Blue);
-                    break;
-                case 3:
-                    showText($"{num} кг = {num * 1000} г", ConsoleColor.Yellow);
-                    showText($"{num} ц = {num * 100} кг", ConsoleColor.Green);
-                    showText($"{num} т = {num * 10} ц", ConsoleColor.Cyan);
-                    showText($"{num} т = {num * 1000} кг", ConsoleColor.Magenta);
-                    break;
-                default:
-                    showText($"{num} т = {num * 1000} кг", ConsoleColor.DarkRed);
-                    break;
-            }
-            showText("-------------------------------", ConsoleColor.White);
+            mh = new MessageHelper();
         }
         public void NumberThree()
         {
-            int menu = 0;
-            int module = 0;
-
-            double number1 = 0;
-            double number2 = 0;
-
-            showText("--Дробові числа писати через кому---");
-            showText("---Потрібно використовувати значення по модулю?---" +
-                "\n------------------(1 - Так; 2 - Ні)-------------------");
-            module = Convert.ToChar(Console.ReadLine());
-
-            switch (module)
-            {
-                case '1':
-                    showText("---Добре! Ви вибрали що будете використовувати значення по модулю---", ConsoleColor.Green);
-                      number1 = Math.Abs(number1);
-                      number2 = Math.Abs(number2);
-                    break;
-                case '2':
-                    showText("---Добре! Ви вибрали що не будете використовувати значення по модулю---", ConsoleColor.Red);
-                    break;
-                default:
-                    showText("---Такого варіанту відповід не має---", ConsoleColor.DarkRed);
-                    break;
-            }
-
-            showText("---Введіть перше число---", ConsoleColor.Cyan);
-            number1 = Convert.ToDouble(Console.ReadLine());
-
-            showText("---Введіть друге число---", ConsoleColor.Magenta);
-            number2 = Convert.ToDouble(Console.ReadLine());
-
-            showText("---Яку операцію Ви хочете зробити? Можливі операції:---" +
-                "\n------------------Додавання - 1------------------------" +
-                "\n------------------Віднімання - 2-----------------------" +
-                "\n------------------Множення - 3-------------------------" +
-                "\n------------------Ділення - 4--------------------------" +
-                "\n------------------Корінь числа - 5---------------------" +
-                "\n------------------Зведення числа в ступінь - 6---------" +
-                "\n------------------Відсоток від числа - 7---------------");
-            showText("---Оберіть пункт операції яку ви хочете виконати---", ConsoleColor.Blue);
-            menu = Convert.ToInt32(Console.ReadLine());
-
-            switch (menu)
-            {
-                case 1:
-                    showText($"•Ви зробили операцію додавання. Результат операції: {number1} + {number2} = {number1 + number2}", ConsoleColor.Yellow);
-                    break;
-                case 2:
-                    showText($"•Ви зробили операцію віднімання. Результат операції: {number1} - {number2} = {number1 - number2}", ConsoleColor.Cyan);
-                    break;
-                case 3:
-                    showText($"•Ви зробили операцію множення. Результат операції: {number1} * {number2} = {number1 * number2}", ConsoleColor.DarkYellow);
-                    break;
-                case 4:
-                    if (number2 == 0)
-                    {
-                        showText("•Неможливо ділити на нуль", ConsoleColor.DarkRed);
-                    }
-                    else
-                    {
-                        showText($"•Ви зробили операцію ділення. Результат операції: {number1} / {number2} = {number1 / number2}", ConsoleColor.DarkGreen);
-                    }
-                    break;
-                case 5:
-                    if (number1 < 0 || number2 < 0)
-                    {
-                        showText("•Неможливо винести корінь із від'ємного числа!", ConsoleColor.DarkRed);
-                    }
-                    else
-                    {
-                        showText($"•Ви зробили операцію корінь із числа. Результат операції: Корінь із першого числа: {Math.Sqrt(number1)} Корінь із другого числа: {Math.Sqrt(number2)}", ConsoleColor.Yellow);
-                    }
-                    break;
-                case 6:
-                    showText($"•Ви зробили операцію зведення числа в ступінь. Результат операції: {number1} ^ {number2} = {Math.Pow(number1, number2)}", ConsoleColor.DarkYellow);
-                    break;
-                case 7:
-                    if (number2 < 0)
-                    {
-                        showText("•Відсоток не може бути від'ємним!", ConsoleColor.DarkRed);
-                    }
-                    else
-                    {
-                        showText($"•Ви зробили операцію відсоток від числа. Результат операції: {number2} відсотків від числа {number1} = {number1 / 100 * number2}", ConsoleColor.Cyan);
-                    }
-                    break;
-            }
+            mh = new MessageHelper();
+        }
+        public void NumberFour()
+        {
+            mh = new MessageHelper();
         }
     }
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            Console.OutputEncoding = System.Text.Encoding.UTF8;
-            E e = new E();
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode; 
 
-            e.showText("Виберіть завдання:", ConsoleColor.White);
-            e.showText("[1] Завдання №1", ConsoleColor.Cyan);
-            e.showText("[2] Завдання №2", ConsoleColor.Green);
-            e.showText("[3] Завдання №3", ConsoleColor.Yellow);
-            int menuM = Convert.ToInt32(Console.ReadLine());
+            MessageHelper mh = new MessageHelper();
+            Task task = new Task();
+
+            mh.showText("Виберіть завдання:", ConsoleColor.White);
+            mh.showText("[1] Завдання №1", ConsoleColor.Cyan);
+            mh.showText("[2] Завдання №2", ConsoleColor.Green);
+            mh.showText("[3] Завдання №3", ConsoleColor.Yellow);
+            mh.showText("[4] Завдання №4", ConsoleColor.Blue);
+            mh.showText("[0] Вихід з програми", ConsoleColor.Red);
+            int menuM = MessageHelper.getInt("");
 
             Console.Clear();
 
             switch (menuM)
             {
+                case 0:
+                    mh.showText("Вихід із програми...", ConsoleColor.DarkRed);
+                    Thread.Sleep(1000);
+                    return;
                 case 1:
-                    e.NumberOne();
+                    mh.showText("Умова:" +
+                        "\n1)", ConsoleColor.Gray);
+                    task.NumberOne();
                     break;
                 case 2:
-                    e.NumberTwo();
+                    mh.showText("Умова:" +
+                        "\n2)", ConsoleColor.Gray);
+                    task.NumberTwo();
                     break;
                 case 3:
-                    e.NumberThree();
+                    mh.showText("Умова:" +
+                        "\n3)", ConsoleColor.Gray);
+                    task.NumberThree();
+                    break;
+                case 4:
+                   mh.showText("Умова:" +
+                        "\n4)", ConsoleColor.Gray);
+                    task.NumberFour();
                     break;
                 default:
-                    e.showText("Виберіть завдання:", ConsoleColor.White);
-                    e.showText("[1] Завдання №1", ConsoleColor.Cyan);
-                    e.showText("[2] Завдання №2", ConsoleColor.Green);
-                    e.showText("[3] Завдання №3", ConsoleColor.Yellow);
-                    menuM = Convert.ToInt32(Console.ReadLine());
+                    mh.showText("Виберіть завдання:", ConsoleColor.White);
+                    mh.showText("[1] Завдання №1", ConsoleColor.Cyan);
+                    mh.showText("[2] Завдання №2", ConsoleColor.Green);
+                    mh.showText("[3] Завдання №3", ConsoleColor.Yellow);
+                    mh.showText("[4] Завдання №4", ConsoleColor.Blue);
+                    mh.showText("[0] Вихід з програми", ConsoleColor.Red);
+                    menuM = MessageHelper.getInt("");
 
 
                     Console.Clear();
